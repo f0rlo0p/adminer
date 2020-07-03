ca702c80a28e73efefed9198289bc5ac4471afae
Khaled Nassar <45688522+knassar702@users.noreply.github.com>
2020-07-03 14:31:47 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from flask import Flask,request,abort
+import subprocess
+app = Flask(__name__)
+
+black_list = [''] # add your blacklist here
+# for example | dangerous file | /etc/passwd 
+
+@app.route('/')
+def index():
+  r = request.args.get('file','')
+  if r not in black_list:
+    return cmd(f'cat {r}')
+  else:
+    return abort(403)
+def command(cmd):
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+        (out, err) = proc.communicate()
+        return out
+        
+if __name__ == '__main__':
+  app.run()
