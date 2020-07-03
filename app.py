9db1fbaee1622e26977dcacf5cdbbb2b74844d2f
Khaled Nassar <45688522+knassar702@users.noreply.github.com>
2020-07-03 14:58:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python3
-from flask import Flask,request,abort,redirect
+from flask import Flask,request,abort
 import subprocess
 app = Flask(__name__)
 
-black_list = [''] # add your blacklist here
+black_list = ['flag.txt'] # add your blacklist here
 # for example | dangerous file | /etc/passwd 
 
+@app.errorhandler(404)
+def show_404(e):
+        return '<h3> <a href="https://github.com/f0rlo0p/thereader">TheReader </a>v0.6</h3><hr><br><h4> Status : 404 </h4>'
 @app.route('/')
 def index():
   r = request.args.get('file','')
-  if r =='':
-    return redirect('/?file=hi.txt')
   if r not in black_list:
-    return command(f'cat {r}').decode('utf-8')
+    return command(f'cat {r}').decode()
   else:
     return abort(403)
 def command(cmd):
         proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
         (out, err) = proc.communicate()
         return out
-        
+
 if __name__ == '__main__':
-  app.run()
+  app.run(port=9090)
