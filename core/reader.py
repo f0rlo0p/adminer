83701d0ba2b8cb5c4a2b0e2dd1394b678a248f56
Khaled Nassar <knassar702@gmail.com>
2020-09-19 23:59:57 +0200

    read func

--- core/reader.py
+++ core/reader.py
@@ -1,23 +1,8 @@
 #!/usr/bin/env python3
-from flask import Flask,request,abort
-import subprocess
-app = Flask(__name__)
-parameter = '' # your parameter . for example : file
-# localhost/?data=hi.txt
-black_list = 'example.txt' # add your blacklist here
-# for example | dangerous file | /etc/passwd 
+from libs import cmd
 
-@app.errorhandler(404)
-def show_404(e):
-        return '<h3> <a href="https://github.com/f0rlo0p/thereader">TheReader </a>v0.6</h3><hr><br><h4> Status : 404 </h4>'
-@app.route('/')
-def index():
-  r = request.args.get(parameter,'')
-  return command(f'cat {r.replace(black_list,"").replace("*","")}').decode()
-def command(cmd):
-        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
-        (out, err) = proc.communicate()
-        return out
+def now(filename):
+    f = open(filename,'r')
+    return f.read()
 
-if __name__ == '__main__':
-  app.run(port=9090)
+
--- core/request.py
+++ core/request.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python3
+import requests
 
+def get():
+    try:
+        r = requests.get('http://google.com')
+        return f'[!] {r.status_code}'
+    except:
+        return 'Connection Error'
